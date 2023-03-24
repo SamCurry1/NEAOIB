5951f7ee44ef17d838419286442186e0908ca306
MoyaA <MoyaA@WALTER>
2023-03-24 12:55:59 +0000

    Add project files.

---
+++ NEAOIB/Controllers/HomeController.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NEAOIB.Views.Home
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Search()
+        {
+            return View();
+        }
+        public ActionResult Bedfordshire()
+        {
+            return View();
+        }
+        public ActionResult Berkshire()
+        {
+            return View();
+        }
+        public ActionResult Bristol()
+        {
+            return View();
+        }
+        public ActionResult Buckinghamshire()
+        {
+            return View();
+        }
+        public ActionResult Cambridgeshire()
+        {
+            return View();
+        }
+        public ActionResult Cheshire()
+        {
+            return View();
+        }
+        public ActionResult Cornwall()
+        {
+            return View();
+        }
+        public ActionResult Cumbria()
+        {
+            return View();
+        }
+        public ActionResult Derbyshire()
+        {
+            return View();
+        }
+        public ActionResult Devon()
+        {
+            return View();
+        }
+        public ActionResult Dorset()
+        {
+            return View();
+        }
+        public ActionResult Durham()
+        {
+            return View();
+        }
+        public ActionResult EastSussex()
+        {
+            return View();
+        }
+        public ActionResult Essex()
+        {
+            return View();
+        }
+        public ActionResult Gloucestershire()
+        {
+            return View();
+        }
+        public ActionResult GreaterLondon()
+        {
+            return View();
+        }
+        public ActionResult GreaterManchester()
+        {
+            return View();
+        }
+        public ActionResult Hampshire()
+        {
+            return View();
+        }
+        public ActionResult Herefordshire()
+        {
+            return View();
+        }
+        public ActionResult Hertfordshire()
+        {
+            return View();
+        }
+        public ActionResult IsleOfWight()
+        {
+            return View();
+        }
+        public ActionResult Kent()
+        {
+            return View();
+        }
+        public ActionResult Lancashire()
+        {
+            return View();
+        }
+        public ActionResult Leicestershire()
+        {
+            return View();
+        }
+        public ActionResult Lincolnshire()
+        {
+            return View();
+        }
+        public ActionResult CityOfLondon()
+        {
+            return View();
+        }
+        public ActionResult Merseyside()
+        {
+            return View();
+        }
+        public ActionResult Norfolk()
+        {
+            return View();
+        }
+        public ActionResult Northamptonshire()
+        {
+            return View();
+        }
+        public ActionResult Northumberland()
+        {
+            return View();
+        }
+        public ActionResult NorthYorkshire()
+        {
+            return View();
+        }
+        public ActionResult Nottinghamshire()
+        {
+            return View();
+        }
+        public ActionResult Oxfordshire()
+        {
+            return View();
+        }
+        public ActionResult Rutland()
+        {
+            return View();
+        }
+        public ActionResult Shropshire()
+        {
+            return View();
+        }
+        public ActionResult Somerset()
+        {
+            return View();
+        }
+        public ActionResult SouthYorkshire()
+        {
+            return View();
+        }
+        public ActionResult Staffordshire()
+        {
+            return View();
+        }
+        public ActionResult Suffolk()
+        {
+            return View();
+        }
+        public ActionResult Surrey()
+        {
+            return View();
+        }
+        public ActionResult TyneAndWear()
+        {
+            return View();
+        }
+        public ActionResult Warwickshire()
+        {
+            return View();
+        }
+        public ActionResult WestMidlands()
+        {
+            return View();
+        }
+        public ActionResult WestSussex()
+        {
+            return View();
+        }
+        public ActionResult WestYorkshire()
+        {
+            return View();
+        }
+        public ActionResult Wiltshire()
+        {
+            return View();
+        }
+        public ActionResult Worcestershire()
+        {
+            return View();
+        }
+        public ActionResult Yorkshire()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ NEAOIB/Models/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace NEAOIB.Models
+{
+    public class Account
+    {
+        [Key] 
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage ="First Name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage ="E-Mail is required.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage ="Please confrm your password.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
